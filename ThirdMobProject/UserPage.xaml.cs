139bd1e0fd424861a911f964c6fae40e9a6f772c
Sasidka <johnnglory001@gmail.com>
2022-02-21 14:45:57 +0300

    First Mobile Project

---
+++ ThirdMobProject/UserPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThirdMobProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserPage : ContentPage
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignInBtn_Clicked(object sender, EventArgs e)
+        {
+            
+            var login = "Said445";
+            var password = "Fur";
+            if ( LogEntry.Text == login && PassEntry.Text == password)
+            {
+                //навигация к странице ProfilePage
+                Navigation.PushAsync(new ProfilePage());
+            }
+            else
+            {
+                //Всплывающее уведомление
+                DisplayAlert("Ошибка", "Введен неправильный логин или пароль", "Ок");
+            }
+        }
+    }
+}
